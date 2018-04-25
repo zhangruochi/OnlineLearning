9af4d0ffdb78c9378c93e19fc16d04e55e44fd60
zhangruochi <zrc720@gmail.com>
2018-04-26 00:24:55 +0800

    2018-04-26

---
+++ Java/week3/csvParser/CsvSolver.java
@@ -0,0 +1,60 @@
+
+/**
+ * Write a description of CsvSolver here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+import edu.duke.*;
+import org.apache.commons.csv.*;
+
+public class CsvSolver {
+
+    public void tester(){
+        FileResource fr = new FileResource();
+        CSVParser parser = fr.getCSVParser();
+        //countryInfo(parser,"Nauru");
+        //parser = fr.getCSVParser();
+        //listExportersTwoProducts(parser,"gold","diamonds");
+        //parser = fr.getCSVParser();
+        bigExporters(parser,"$999,999,999,999");
+        //parser = fr.getCSVParser();
+        //System.out.println(sugerNum(parser));
+       
+    }
+
+    public String countryInfo(CSVParser parser, String country){
+        for(CSVRecord record : parser ){
+            if(record.get("Country").equals(country))
+                System.out.println(record.get("Country") +": " + record.get("Exports")+ ": "+ record.get("Value (dollars)"));
+        }
+        return "NOT FOUND";
+    }
+
+    public void listExportersTwoProducts(CSVParser parser, String exportItem1, String exportItem2){
+        for(CSVRecord record : parser ){
+            if(record.get("Exports").contains(exportItem1) && record.get("Exports").contains(exportItem2))
+                System.out.println(record.get("Country"));
+        }   
+    }
+
+    public void bigExporters(CSVParser parser, String amount){
+        for(CSVRecord record : parser ){
+            if(record.get("Value (dollars)").length() > amount.length()){
+                System.out.println(record.get("Country") + " " + record.get("Value (dollars)"));
+            } 
+        }        
+    }
+    
+    public int sugerNum(CSVParser parser){
+        int count= 0;
+        for(CSVRecord record : parser ){
+            if(record.get("Exports").contains("sugar")){
+               count++;
+            } 
+        } 
+        return count;
+    }
+    
+    
+}
